baf2e6bf9f01359edb51a28e2e1cd3c3569bf14d
micmic541 <mk0039unicorn@gmail.com>
2021-08-17 21:45:25 +0900

    SQLインジェクション対策#4

--- www/model/cart.php
+++ www/model/cart.php
@@ -70,10 +70,10 @@ function insert_cart($db, $user_id, $item_id, $amount = 1){
         user_id,
         amount
       )
-    VALUES({$item_id}, {$user_id}, {$amount})
+    VALUES( ?, ?, {$amount})
   ";
 
-  return execute_query($db, $sql, [$user_id, $item_id]);
+  return execute_query($db, $sql, [$item_id, $user_id]);
 }
 
 function update_cart_amount($db, $cart_id, $amount){
